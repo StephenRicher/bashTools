c43309c55b8efb3fc829fee47e6ded14aa29e480
Stephen Richer <sr467@bath.ac.uk>
2019-12-05 17:30:09 +0000

    Modified to check argument input and output error message.

--- is_dir.sh
+++ is_dir.sh
@@ -3,7 +3,19 @@
 is_dir() {
     local dir="${1}"
 
-    [[ -d "${dir}" ]]
+    all_empty "${dir}" && fail "Error: No arguments provided."
+
+    if [[ ! -d "${dir}" ]]; then
+        >&2 echo ""${dir}" is not a directory."
+        ec=1
+    fi
+
+    exit "${ec-0}"
+}
+
+fail() {
+    >&2 echo "${1}"
+    exit "${2-1}"
 }
 
 is_dir "${@}"
